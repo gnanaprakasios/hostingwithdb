be9b7c07bf420c96191d59f68386fa2647ec4c55
gnana <gnana.prakasi@christuniversity.in>
2025-09-01 14:49:32 +0530

    Node MySQL server for Render

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const mysql = require("mysql2");
 
 // MySQL connection
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "123456",
-    database: "testdb"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
 });
 
 db.connect(err => {
